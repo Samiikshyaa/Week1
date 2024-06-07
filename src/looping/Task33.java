d70711559ebe16bfdac9b9c4efc12b1c9b636816
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-07 11:00:59 +0545

    June 7

--- src/looping/Task33.java
+++ src/looping/Task33.java
@@ -3,8 +3,30 @@ package looping;
 public class Task33 {
     public static void main(String[] args) {
         char ch = '*';
-
-//        ti
-
+        int n = 5, times = 1, scount = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= scount; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= times; j++) {
+                System.out.print(ch);
+            }
+            times += 2;
+            scount --;
+            System.out.println();
+        }
+        scount = 1;
+        times = 7;
+        for (int i = 1; i<5 ; i++){
+            for (int j = 1; j <= scount ; j++) {
+                System.out.print(" ");
+            }
+            scount++;
+            for (int k = times; k >= 1 ; k--) {
+                System.out.print(ch);
+            }
+            times = times -2;
+            System.out.println();
+        }
     }
 }
--- src/looping/Task34.java
+++ src/looping/Task34.java
@@ -10,8 +10,8 @@ public class Task34 {
             }
             System.out.println();
         }
-        for (int i = 1; i <= n ; i++) {
-            for (int j = i; j <= n; j++) {
+        for (int i = 1; i <= n-1 ; i++) {
+            for (int j = i; j <= n-1; j++) {
                 System.out.print(ch+" ");
             }
             System.out.println();
