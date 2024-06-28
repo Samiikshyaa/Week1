e85b8cf6058536593df178e4714431afe463230b
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-28 23:28:43 +0545

    Task 40

---
+++ src/Day2/Task40.java
@@ -0,0 +1,21 @@
+package Day2;
+
+import java.util.Arrays;
+
+public class Task40 {
+//    Write a Java program to get the contents of a given string as a character array.
+    public static void main(String[] args) {
+        String s = "Hello world!";
+        char[] c = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            c[i] = s.charAt(i);
+        }
+        System.out.println(Arrays.toString(c));
+
+        // or
+        char[] charray = s.toCharArray();
+        System.out.println(Arrays.toString(charray));
+    }
+}
+
