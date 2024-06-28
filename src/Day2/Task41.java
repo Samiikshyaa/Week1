dc9000e9d7f36e7f4d0190beb8ad0ec1d52f6e8e
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-06-28 23:38:32 +0545

    Task 41

---
+++ src/Day2/Task41.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class Task41 {
+//    Write a Java program to create a unique identifier of a given string.
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.hashCode());
+    }
+}
