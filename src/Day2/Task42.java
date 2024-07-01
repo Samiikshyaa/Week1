f1c04c3db30729f55ef1edb9fb12e527c151801f
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-07-01 23:13:07 +0545

    Added

---
+++ src/Day2/Task42.java
@@ -0,0 +1,8 @@
+package Day2;
+
+public class Task42 {
+    //    Write a Java program to get the index of all the characters of the alphabet.
+    public static void main(String[] args) {
+    
+    }
+}
