6f94acd1b27272be3cd4440be58a104a42960392
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-07-07 20:06:47 +0545

    Task51

--- src/Day2/Task51.java
+++ src/Day2/Task51.java
@@ -1,7 +1,14 @@
 package Day2;
 
+import java.util.Arrays;
+
 public class Task51 {
     public static void main(String[] args) {
 //        Write a Java program to create a character array containing the contents of a string.
+        String s = "Samikshya";
+        char[] c = s.toCharArray();
+        System.out.println("The character array is: "+ Arrays.toString(c));
+        System.out.println("The String is: "+s);
+
     }
 }
