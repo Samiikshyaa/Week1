53682f554e674937740aef69ca09dcaedcae6c79
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-07-07 23:17:12 +0545

    Task 54

---
+++ src/Day2/Task52_53.java
@@ -0,0 +1,13 @@
+package Day2;
+
+import java.io.StringReader;
+
+public class Task52_53 {
+    public static void main(String[] args) {
+//        Write a Java program to convert all the characters in a string to lowercase.
+//        Write a Java program to convert all the characters in a string to uppercase.
+        String s = "Samikshya";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
---
+++ src/Day2/Task54.java
@@ -0,0 +1,25 @@
+package Day2;
+
+public class Task54 {
+    public static void main(String[] args) {
+//        Write a Java program to trim any leading or trailing whitespace from a given string.
+        String s = " Samikshya ";
+        StringBuilder sb = new StringBuilder();
+        System.out.println(s);
+        System.out.println(s.length());
+        if(Character.isWhitespace(s.charAt(0)) || Character.isWhitespace(s.charAt(s.length()-1))){
+            for (int i = 0; i < s.length()-2; i++){
+                sb.append(s.charAt(i+1));
+            }
+        }
+        System.out.println(sb.length());
+        System.out.println(sb);
+
+        //    oR========================================
+        String nex = " Hello World ";
+        System.out.println("Before:"+ nex.length());
+        nex = nex.trim();
+        System.out.println(nex);
+        System.out.println(nex.length());
+    }
+}
